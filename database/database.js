f90639e504c3b9ff30670d7fbf81453a15e96a92
nam-ward <dragondroid321@gmail.com>
2021-02-05 13:06:24 -0300

    teste

--- database/database.js
+++ database/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const connection = new Sequelize('heroku_47a5ae6fdbd1e5b','/b6cd6ff4c8233a','9b384363',{
+const connection = new Sequelize('heroku_47a5ae6fdbd1e5b','b6cd6ff4c8233a','9b384363',{
     host: 'us-cdbr-east-03.cleardb.com',
     dialect: 'mysql'
 });
